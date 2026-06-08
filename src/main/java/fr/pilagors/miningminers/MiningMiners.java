f0dfacf07f970967ebc378c97f4fb9bd4eddd72e
Lucas Monfort <Lucas_Monfort@etu.ube.fr>
2026-06-08 17:04:35 +0200

    Initial Commit

---
+++ src/main/java/fr/pilagors/miningminers/MiningMiners.java
@@ -0,0 +1,51 @@
+package fr.pilagors.miningminers;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.slf4j.Logger;
+
+@Mod(MiningMiners.MODID)
+public final class MiningMiners {
+    public static final String MODID = "miningminers";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+
+    public MiningMiners(FMLJavaModLoadingContext context) {
+        var modBusGroup = context.getModBusGroup();
+
+        FMLCommonSetupEvent.getBus(modBusGroup).addListener(this::commonSetup);
+
+        BLOCKS.register(modBusGroup);
+        ITEMS.register(modBusGroup);
+        CREATIVE_MODE_TABS.register(modBusGroup);
+
+        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        LOGGER.info("Mining Miners - Common Setup");
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            LOGGER.info("Mining Miners - Client Setup");
+        }
+    }
+}
